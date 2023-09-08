a09beb8493b91032ab27e97ee29a44fce6a1aca9
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2023-09-08 15:38:27 -0300

    feat: :wrench: criar o arquivo do docker-compose da api

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('vendedor')->group(function () {
+    Route::get('/', function () {
+        return response()->json(["msg" => "ok"], 200);
+    });
+});
